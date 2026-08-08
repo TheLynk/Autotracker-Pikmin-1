627a20850ffb5905dfcaaacca02030881f4cde2e
TheLynk <jeanpol4226@gmail.com>
2026-08-08 13:35:07 +0200

    Big Rework

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,5 +1,35 @@
 -- entry point for all lua code of the pack
 -- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
+
+Tracker:AddItems("items/items.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/pikmin_group_visible.json") -- état par défaut
+Tracker:AddLayouts("layouts/areas_accessible_group_visible.json")
+
+local function updatePikminGroup()
+    local item = Tracker:FindObjectForCode("shuffle_pikmin") -- vérifie le code exact dans items.json
+    if item and item.Active then
+        Tracker:AddLayouts("layouts/pikmin_group_visible.json")
+    else
+        Tracker:AddLayouts("layouts/pikmin_group_hidden.json")
+    end
+end
+
+ScriptHost:AddWatchForCode("pikmin_group_watch", "shuffle_pikmin", updatePikminGroup)
+updatePikminGroup()
+
+local function updateAreasAccessibleGroup()
+    local item = Tracker:FindObjectForCode("shuffle_map") -- vérifie le code exact dans items.json
+    if item and item.Active then
+        Tracker:AddLayouts("layouts/areas_accessible_group_visible.json")
+    else
+        Tracker:AddLayouts("layouts/areas_accessible_group_hidden.json")
+    end
+end
+
+ScriptHost:AddWatchForCode("areas_accessible_group_watch", "shuffle_map", updateAreasAccessibleGroup)
+updateAreasAccessibleGroup()
+
 ENABLE_DEBUG_LOG = true
 -- get current variant
 local variant = Tracker.ActiveVariantUID
@@ -27,11 +57,11 @@ Tracker:AddMaps("maps/maps.jsonc")
 
     -- Locations
 Tracker:AddLocations("locations/world_map.jsonc")
-Tracker:AddLocations("locations/crash_site.jsonc")
-Tracker:AddLocations("locations/forest_of_hope.jsonc")
-Tracker:AddLocations("locations/forest_navel.jsonc")
-Tracker:AddLocations("locations/distant_spring.jsonc")
-Tracker:AddLocations("locations/final_trial.jsonc")
+Tracker:AddLocations("locations/the_impact_site.jsonc")
+Tracker:AddLocations("locations/the_forest_of_hope.jsonc")
+Tracker:AddLocations("locations/the_forest_navel.jsonc")
+Tracker:AddLocations("locations/the_distant_spring.jsonc")
+Tracker:AddLocations("locations/the_final_trial.jsonc")
 
 -- Layout
 Tracker:AddLayouts("layouts/items.jsonc")
