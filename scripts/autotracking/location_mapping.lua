9c2dbf55ce054870da6df20cd78ac94fd59cb70e
TheLynk <jeanpol4226@gmail.com>
2026-08-11 02:58:37 +0200

    Add Location TFT & AutoTracking TDS & TFT

--- scripts/autotracking/location_mapping.lua
+++ scripts/autotracking/location_mapping.lua
@@ -20,6 +20,7 @@ LOCATION_MAPPING = {
 
 
 -- The Forest Navel
+
 [71428] = { {"@The Forest Navel/Analog Computer/Ship Part"} },
 [71420] = { {"@The Forest Navel/Automatic Gear/Ship Part"} },
 [71414] = { {"@The Forest Navel/#1 Ionium Jet/Ship Part"} },
@@ -32,8 +33,20 @@ LOCATION_MAPPING = {
 
 -- The Distant Spring
 
+[71400] = { {"@The Distant Spring/Bowsprit/Ship Part"} },
+[71401] = { {"@The Distant Spring/Gluon Drive/Ship Part"} },
+[71406] = { {"@The Distant Spring/Interstellar Radio/Ship Part"} },
+[71408] = { {"@The Distant Spring/Chronos Reactor/Ship Part"} },
+[71415] = { {"@The Distant Spring/#2 Ionium Jet/Ship Part"} },
+[71418] = { {"@The Distant Spring/Pilot's Seat/Ship Part"} },
+[71421] = { {"@The Distant Spring/Zirconium Rotor/Ship Part"} },
+[71423] = { {"@The Distant Spring/Repair-Type Bolt/Ship Part"} },
+[71425] = { {"@The Distant Spring/Massage Machine/Ship Part"} },
+[71429] = { {"@The Distant Spring/UV Lamp/Ship Part"} },
+
 -- The Final Trial
 
+[71426] = { {"@The Final Trial/Secret Safe/Ship Part"} },
 -- Upgrade Dolphin
 
 -- Achievement
